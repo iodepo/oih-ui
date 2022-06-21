80fbefd65869ccf65f1dc1d3c5d515f1965a44f2
Cormac Hallinan <cormachallinan@derilinx.com>
2022-06-21 12:43:04 +0100

    adding facets for API

--- api/main.py
+++ api/main.py
@@ -18,19 +18,43 @@ app.add_middleware(
     allow_origins=['*']
 )
 
+##### TEMP CODE #####
+import json
+
+# Opening JSON file
+f = open('/Users/cormac/PycharmProjects/unesco-oih/api/sample_result.json')
+# f = open('/usr/src/app/api/sample_result.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+##### TEMP CODE #####
 
 @app.get("/search")
 async def search(text, document_type: str = None):
-    search_text = urllib.parse.unquote(text)
-    solr_search_query = SolarSearchQueryBuilder(
-        Search(search_text, document_type)
-    )
-    solr_search_query.add_search_fq()
-    solr_search_query.add_facet_fields()
-    res = requests.get(SOLR_URL, params=solr_search_query.params)
-    data = res.json()
+    #### GOOD CODE #####
+    # search_text = urllib.parse.unquote(text)
+    # solr_search_query = SolarSearchQueryBuilder(
+    #     Search(search_text, document_type)
+    # )
+    # solr_search_query.add_search_fq()
+    # solr_search_query.add_facet_fields()
+    # res = requests.get(SOLR_URL, params=solr_search_query.params)
+    # data = res.json()
+    # response = {'docs': data['response']['docs']}
+    #### GOOD CODE #####
     response = {'docs': data['response']['docs']}
     response.update(await _convert_counts_array_to_response_dict(data['facet_counts']['facet_fields']['type']))
+    response['facets'] = []
+    for facet in ['txt_knowsAbout', 'txt_knowsLanguage', 'txt_nationality', 'txt_jobTitle', 'txt_contributor', 'txt_keywords',
+                  'txt_memberOf', 'txt_parentOrganization', 'id_provider', 'id_includedInDataCatalog', 'keys']:
+        if data['facet_counts']['facet_fields'][facet]:
+            response['facets'].append(
+                {
+                    "name": facet,
+                    "counts": await _convert_facet_counts(
+                        data['facet_counts']['facet_fields'][facet])
+                })
     return response
 
 
@@ -50,3 +74,13 @@ async def _convert_counts_array_to_response_dict(counts):
     for index in range(0, len(counts), 2):
         response['counts'][counts[index]] = counts[index + 1]
     return response
+
+
+async def _convert_facet_counts(facet_counts):
+    facet_response = []
+    for index in range(0, len(facet_counts), 2):
+        facet_response.append({
+            'name': facet_counts[index],
+            'count': facet_counts[index + 1]
+        })
+    return facet_response
