2bb4a1febfda40e26de6bfea07d483c59b877203
Eric Soroos <eric@derilinx.com>
2022-07-05 14:22:37 +0000

    refactor, new params in the query builder

--- api/util/solr_query_builder.py
+++ api/util/solr_query_builder.py
@@ -6,7 +6,9 @@ DEFAULT_FACET_FIELDS = ['txt_knowsAbout', 'name', 'txt_knowsLanguage', 'txt_nati
 
 class SolrQueryBuilder:
 
-    def __init__(self, rows=10, facet_min_count=1, start=0, query='*:*', sort='score desc, indexed_ts desc', facet='true'):
+    def __init__(self, rows=10, facet_min_count=1, start=0,
+                 query='*:*', sort='score desc, indexed_ts desc',
+                 facet='true', flList=None):
         self.params = dict()
         self.params['q'] = query
         self.params['sort'] = sort
@@ -14,14 +16,24 @@ class SolrQueryBuilder:
         self.params['facet.mincount'] = facet_min_count
         self.params['start'] = start
         self.params["facet"] = facet
-
+        if flList:
+            self.params['fl'] = ','.join(flList)
+        
     def add_facet_fields(self, facet_fields=None):
-        self.params["facet.field"] = facet_fields if facet_fields else DEFAULT_FACET_FIELDS
+        self.params["facet.field"] = facet_fields if facet_fields is not None else DEFAULT_FACET_FIELDS
 
+
+    formats = {
+        'text': '+%(name)s:(%(value)s)',
+        'the_geom': '+%(name)s:%(value)s',
+        }
+
+    def _fmt(self, name):
+        return self.formats.get(name, '+%(name):"%(value)"')
+    
     def add_fq(self, name, value):
         if "fq" not in self.params:
             self.params['fq'] = []
-        if name == 'text':
-            self.params['fq'].append(f'+{name}:({value})')
-        else:
-            self.params['fq'].append(f'+{name}:"{value}"')
+            
+        self.params['fq'].append(self._fmt(name) % {'name': name, 'value':value})
+        
