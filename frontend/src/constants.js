0597c992e45d35eddfb5fafeb5cadcf7f945c8b3
Cormac Hallinan <cormachallinan@derilinx.com>
2022-07-20 11:29:57 +0100

    adding constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,18 @@
+const regionBoundsMap = {
+    Atlantic_Ocean: '[5.483746321637085,-110.11658847394503 TO 62.204953479609145,30.116588473945995]',
+    Europe: '[10,-60 TO 73,125]',
+    Pacific_Ocean: '[-21,77 TO 65,278]',
+    Africa: '[-41,-58 TO 47,127]',
+    Latin_America_and_the_Caribbean: '[-35.97799165355438,-131.86094382957086 TO 14.758199586212825,-31.1390561704267]',
+    Mediterranean_Sea: '[29,-8 TO 48,39]',
+    Indian_Ocean: '[-46,-19 TO 46,175]',
+    Caribbean_Sea: '[13,-98 TO 35,-52]',
+    Asia: '[-12,14 TO 65,199]',
+    Americas: '[-23,-170 TO 60,15]',
+    Southern_Ocean: '[-75,-76 TO -18,109]',
+    Pacific_Small_Islands: '[-24,-219 TO 24,-128]',
+    Arctic_Ocean: '[64,-42 TO 85,140]',
+    Oceania: '[-45,113 TO -2,204]',
+}
+
+export default regionBoundsMap
