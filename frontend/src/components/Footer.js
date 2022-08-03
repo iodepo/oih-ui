85be75626754fb4d120f08b0a78c9b53dac937b7
Jenom Kassim <jenomkassim@derilinx.com>
2022-08-03 10:55:46 +0100

    Added logos to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,3 +1,9 @@
+import OIHLogo from '../resources/Logo Nav x 2.png'
+import IODE from '../resources/IODE Logo Footer.png'
+import UNESCO from '../resources/Unesco Logo White Footer.png'
+import UNESCOScience from '../resources/UNESCO Science White Logo Footer.png'
+import Vlaanderen from '../resources/Vlaanderen Logo White Footer.png'
+
 export default function Footer() {
     return (
         <div className="mt-5 ml-5 pt-5 mb-0 pb-5 text-light primary-bg">
@@ -9,6 +15,25 @@ export default function Footer() {
                 <div className="flex-fill">
                     <h3 className="primary-color-alt fw-bold h4 text-uppercase">Want to learn more?</h3>
                     <button className="w-50 btn rounded-0 btn-info text-dark">GET IN TOUCH</button>
+                </div>
+            </div>
+            <div className="container">
+                <div className="d-flex mt-5">
+                    <div className="col">
+                        <img src={UNESCO} className="img-fluid footer-logos" alt="OIH Logo" />
+                    </div>
+                    <div className="col">
+                        <img src={UNESCOScience} className="img-fluid footer-logos" alt="OIH Logo" />
+                    </div>
+                    <div className="col">
+                        <img src={IODE} className="img-fluid footer-logos" alt="OIH Logo" />
+                    </div>
+                     <div className="col">
+                        <img src={Vlaanderen} className="img-fluid footer-logos" alt="OIH Logo" />
+                    </div>
+                    <div className="col">
+                        <img src={OIHLogo} className="img-fluid footer-logos" alt="OIH Logo" />
+                    </div>
                 </div>
             </div>
         </div>
