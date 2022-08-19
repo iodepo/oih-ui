29a83e791fc42b05f646181a6efba645f0435a82
Jenom Kassim <jenomkassim@derilinx.com>
2022-08-19 11:27:34 +0100

    WIP: Styling results tab

--- frontend/src/components/ResultTabs.js
+++ frontend/src/components/ResultTabs.js
@@ -18,16 +18,19 @@ export default function ResultTabs({
   return (
     <div id='ResTabs' >
         <Tabs selectedIndex={tabList.findIndex(tab => tab.title === searchType)}>
-          <TabList className="bg-light rounded-2 pt-5 border-bottom text-uppercase text-secondary">
+          <TabList className="rounded-2 pt-5 border-bottom text-uppercase text-secondary">
             {tabList.map(tab =>
                 <Tab
                   onClick={changeSearchType(tab.title)}
                   key={tab.title}
-                  selectedClassName="bg-light border border-bottom-0 rounded-0 primary-color-alt"
+                  selectedClassName=" rounded-0 primary-color-alt"
                 >
-                  <span selected={tab.title === searchType}>
-                    <span className="h6 fw-bold p-1"> {tab.tab_name} </span> {counts[tab.title] && <span className="badge bg-secondary">{formatter.format(counts[tab.title])}</span>}
-                  </span>
+                  <div selected={tab.title === searchType} className="d-flex">
+                    <div className="primary-bg rounded-circle bubble-alt">{formatter.format(counts[tab.title])}</div>
+
+                    {counts[tab.title] &&
+                    <div className="text-light fw-bold bubble-textarea-alt text-capitalize"> {tab.tab_name} </div>}
+                  </div>
                 </Tab>
             )}
           </TabList>
