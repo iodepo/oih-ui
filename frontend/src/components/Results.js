4dc3b1e7b6381c96762e902cdaf84dfef0eb7c4b
Eric Soroos <eric@derilinx.com>
2022-08-22 13:24:10 +0000

    Rely on the returned counts rather than the facet

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -150,11 +150,9 @@ export default function Results() {
             .then(json => {
                 setResults(json.docs);
                 setFacets(json.facets.filter(facet => facet.counts.length > 0));
-                // TODO: Pagination will have *issues* with double-counted orgs+experts
-                // (count of orgs, people, and experts are summed, double-counting both orgs and people)
-                const count = Object.values(json.counts).reduce((x, y) => x + y, 0);
+                const count = json.count;
                 setResultCount(count);
-                setCounts(prev => ({ ...prev, [searchType]: count }))
+                setCounts(prev => ({ ...prev, [searchType]: count }));
             })
     }, 1000), [dataServiceUrl, searchText, searchType, region, facetQuery])
 
@@ -176,9 +174,9 @@ export default function Results() {
                 .then(response => response.json())
                 .then(json => {
                     setResults(json.docs);
-                    const count = json.counts[searchType]
+                    const count = json.count;
                     setResultCount(count);
-                    setCounts(prev => ({ ...prev, [searchType]: count }))
+                    setCounts(prev => ({ ...prev, [searchType]: count }));
                     setFacets(json.facets.filter(facet => facet.counts.length > 0));
                 })
 
