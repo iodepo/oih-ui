558146fbf3016f8d47746e87941287ffdf4fcf09
Eric Soroos <eric@derilinx.com>
2022-08-22 09:07:25 +0000

    Fix Flash of NaN in tab list

--- frontend/src/components/ResultTabs.js
+++ frontend/src/components/ResultTabs.js
@@ -18,26 +18,26 @@ export default function ResultTabs({
   return (
     <div id='ResTabs' >
         <Tabs selectedIndex={tabList.findIndex(tab => tab.title === searchType)}>
-          <TabList className="rounded-2 pt-5 border-bottom text-uppercase text-secondary">
+          <TabList className="rounded-2 pt-5 text-secondary">
             {tabList.map(tab =>
                 <Tab
                   onClick={changeSearchType(tab.title)}
                   key={tab.title}
                   selectedClassName=" rounded-0 primary-color-alt"
                 >
-                  <div selected={tab.title === searchType} className="d-flex">
-                    <div className="bubble-container">
-                      <div className={"" + (tab.title === searchType ? 'bg-selected rounded-circle bubble-alt' : ' primary-bg rounded-circle bubble-alt')}>
-                        <span className="text-light-tab">{formatter.format(counts[tab.title])}</span>
-                    </div>
-                    </div>
-
-
-                    {counts[tab.title] &&
-                    <div className={"fw-bold text-capitalize" + (tab.title === searchType ? ' bubble-textarea-selected' : ' bubble-textarea-alt')}>
-                      {tab.tab_name}
-                    </div>}
+                  {counts[tab.title] &&
+                   (<div selected={tab.title === searchType} className="d-flex">
+                      <div className="bubble-container">
+                        <div className={"" + (tab.title === searchType ? 'bg-selected rounded-circle bubble-alt' : ' primary-bg rounded-circle bubble-alt')}>
+                          <span className="text-light-tab">{formatter.format(counts[tab.title])}</span>
+                        </div>
+                      </div>
+                      <div className={"fw-bold text-capitalize" + (tab.title === searchType ? ' bubble-textarea-selected' : ' bubble-textarea-alt')}>
+                        {tab.tab_name}
+                      </div>
                   </div>
+                   )}
+
                 </Tab>
             )}
           </TabList>
