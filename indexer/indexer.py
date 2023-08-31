7ca879a3d44a729fbd77226a6ad45a0f3f964086
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2023-08-31 14:54:39 -0300

    handle type:Project vs ResearchProject

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -146,6 +146,13 @@ def genericType_toAtts(orig, rid=None):
                 _id = hashlib.md5(name.encode('utf-8')).hexdigest()
             else:
                 _id = str(uuid.uuid4())
+                
+        #handle type:Project as type:ResearchProject (see issue#43)
+        if orig['@type'] == 'Project' or orig['@type'] == 'ResearchProject':
+          print('***changing type:Project to type:ResearchProject')
+          projectType = 'ResearchProject'
+        else:
+          projectType = orig['@type']                
 
         data = [
             Att(None, _id, 'id'),
